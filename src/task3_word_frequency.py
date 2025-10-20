ab75d92c2517caf71fd19a988579a9a453cdd2fe
Halyna Melnyk <39273210+mehalyna@users.noreply.github.com>
2025-10-20 18:57:17 +0300

    Change return value to None in word frequency function
    Refactor word frequency calculation to return None instead of a list of tuples.

--- src/task3_word_frequency.py
+++ src/task3_word_frequency.py
@@ -21,32 +21,20 @@ def top_words(text: str, n: int = 3) -> list[tuple[str, int]]:
         List of (word, count) tuples for the top n words
     """
     # Use regex to extract words (letters and digits)
-    words = re.findall(r'\b[a-zA-Z0-9]+\b', text.lower())
+    
     
     # Count word frequencies manually
-    word_counts = {}
-    for word in words:
-        if word in word_counts:
-            word_counts[word] += 1
-        else:
-            word_counts[word] = 1
+    
     
     # Convert to list of tuples
-    word_list = []
-    for word, count in word_counts.items():
-        word_list.append((word, count))
+    
     
     # Sort by count (descending) and then by word (ascending) for ties
     # Using bubble sort to avoid built-in sort
-    for i in range(len(word_list)):
-        for j in range(0, len(word_list) - i - 1):
-            # Sort by count descending, then by word ascending
-            if (word_list[j][1] < word_list[j + 1][1] or 
-                (word_list[j][1] == word_list[j + 1][1] and word_list[j][0] > word_list[j + 1][0])):
-                word_list[j], word_list[j + 1] = word_list[j + 1], word_list[j]
+    
     
     # Return top n words
-    return word_list[:n]
+    return None
 
 
 if __name__ == "__main__":
